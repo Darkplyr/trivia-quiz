9c364f2356258fb87d54e393969cf0b81912ed0f
Darkplyr <74112342+Darkplyr@users.noreply.github.com>
2021-09-05 23:49:19 +0300

    ProgressBar added and Next Button working

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -10,62 +10,60 @@ export class FeatureComponent implements OnInit {
   @Input() question : string = 'question';
   @Input() answer : string = 'answer';
   @Input() choices : any = [];
-  constructor(private apC : AppComponent) { 
-    //apC.callQuestion();
+  constructor(private apC : AppComponent) {
   }
 
   ngOnInit(): void {
-    
+    var nextButton = document.getElementById("nxtBtn");
+    nextButton?.setAttribute("disabled", "true");
   }
 
   checkAnswer(choice : string, answer : string) : void {
-    // working 
-    //console.log(choice);
-    //console.log(answer);
-    //var button = document.getElementById("btn");
-    //if correct => change color to green else change color to red
-    console.log(this.choices);
-    var button = document.getElementById(choice);
-    console.log(button?.innerText);
+    var answerBtn = document.getElementById(choice);
     if(choice === answer)
     {
       console.log("Correct");
-      button?.setAttribute("style", "background-color: #00FF00");
+      answerBtn?.setAttribute("style", "background-color: #00FF00");
+      this.createElement("#00FF00");
     }
     else
     {
       console.log("Wrong");
-      button?.setAttribute("style", "background-color: #FF0000");
+      answerBtn?.setAttribute("style", "background-color: #FF0000");
+      this.createElement("#FF0000");
     }
 
     for (let i = 0; i < this.choices.length; i++) {
-      var button1 = document.getElementById(this.choices[i]);
-      button1?.setAttribute("disabled", "")
+      var choiceBtn = document.getElementById(this.choices[i]);
+      choiceBtn?.setAttribute("disabled", "true")
     }
-    
+
+    var nextButton = document.getElementById("nxtBtn");
+    nextButton?.removeAttribute("disabled");
   }
 
+  nextQuestion() : void{
+    var mainDiv = document.getElementById("mainDiv");
+    this.removeElement("mainDiv");
+    this.apC.callQuestion();
+  }
   
+  removeElement(elementId : any) : void {
+    var element = document.getElementById(elementId);
+    element?.parentNode?.removeChild(element);
+    }
 
-/*function submitAnswer()
-{
-    var radios = document.getElementsByTagName("input");
-    var value;
-    for (var i = 0; i < radios.length; i++)
-    {
-        if(radios[i].type === 'radio' && radios[i].checked)
-        {
-            value = radios[i].value;
-        }
-    }
-    if(value == 0)
-    {
-        alert("Correct Answer");
-    }
-    else
-    {
-        alert("Wrong Asnwer");
-    }
-}*/
-
+  createElement(choice : string) : void {
+    var element = document.createElement("button");
+    var asnwerBtn = document.getElementById(choice);
+    element?.setAttribute("disabled", "true");
+    element?.setAttribute("class", "btn-progBar");
+    element?.setAttribute("style", "width: 10px; background-color: " + choice)
+    console.log("Done!");
+    var body = document.getElementById("progBar");
+    body?.appendChild(element);
+    element.addEventListener ("click", function() {
+      alert("did something");
+    });
+  }
 }
