5ffb8659b5f4c4bad7f3ee0e060b2bee3b993c85
Nadim Mouawad <nadimetto@gmail.com>
2021-09-07 20:29:03 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,6 @@ export class AppComponent {
   correctAnswer : string = ""
   constructor() { 
     this.callQuestion();
-    // fetch('https://opentdb.com/api.php?amount=1&encode=url3986').then(res => res.json())
-    // .then(data => {
-    //   for (let i = 0; i < data.results.length; i++) {
-    //     let f = {
-    //       question : decodeURIComponent(data.results[i].question),
-    //       choices : data.results[i].incorrect_answers,
-    //       answer : decodeURIComponent(data.results[i].correct_answer),
-    //     }
-    //     f.choices = this.decodeArray(f.choices);
-    //     f.choices.splice(Math.floor(Math.random() * 4), 0, f.answer);
-    //     this.features.push(f);
-    //     this.correctAnswer = f.answer;
-    //   }
-    // })
   }
 
   callQuestion() : void
